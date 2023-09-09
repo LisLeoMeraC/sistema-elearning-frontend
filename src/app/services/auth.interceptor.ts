17fcfd685b068a879b68e2d5e08652be91ee8795
RobertoSuarez <electrosonix12@gmail.com>
2023-09-08 23:54:13 -0500

    base url

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -2,18 +2,19 @@ import { LoginService } from './login.service';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import baseURL from './helper';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  private BACKEND_URL = "localhost:8080";
 
   constructor(private loginService: LoginService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // Solo agregar el token si la solicitud es hacia tu backend
-    if (req.url.includes(this.BACKEND_URL)) {
+    if (req.url.includes(baseURL)) {
       const token = this.loginService.getToken();
+      console.log("token: ", token);
       if (token != null) {
         req = req.clone({
           setHeaders: { Authorization: `Bearer ${token}` }
--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,2 +1,2 @@
-let baseURL= 'http://localhost:8080'
+let baseURL= 'https://computev3-a8811c602065.herokuapp.com'
 export default baseURL;
