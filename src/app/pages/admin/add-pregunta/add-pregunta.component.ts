b0a76b0223a479dc5c017e569f2a79b22cd907ab
LisLeoMeraC <84941789+LisLeoMeraC@users.noreply.github.com>
2023-09-18 22:30:39 -0500

    Update

--- src/app/pages/admin/add-pregunta/add-pregunta.component.ts
+++ src/app/pages/admin/add-pregunta/add-pregunta.component.ts
@@ -91,7 +91,7 @@ export class AddPreguntaComponent implements OnInit {
     this.chatgptService
       .generateQuestion(
         `Hazme una pregunta con 4 opciones pero con texto no muy largos mas la respuesta que diga: 
-        Respuesta: sobre el tema de ${this.titulo}`
+        Respuesta: sobre el tema de ${this.titulo} mas la URL en wikipedia de donde se saco la informacion`
       )
       .subscribe(
         (response) => {
